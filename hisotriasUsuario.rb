1434d85916f5a47d82fddb712695084715159130
Koyirox <martinezkind@gmail.com>
2019-07-22 15:53:07 -0400

    avance entrega1 proyecto

---
+++ hisotriasUsuario.rb
@@ -0,0 +1,31 @@
+=begin
+    
+
+
+1) Como visita puedo ver un listado de usuarios registrados en mi plataforma, mostrar su perfil y un preview de su postcast (una muestra)
+
+2) como usuario debo poder escuchar el podcast de otros usuarios.
+
+3) como usuario  debo poder votar (like) por cada podcast de otros usuarios
+
+4) Como usuario puedo guardar los podcast que más me gustan.
+
+5) Como visita / puedes buscar por titulo/autor del podcast 
+
+6) como visita / puedo filtrar por los podcast más votados 
+
+7) Como usuario puedo comentar sobre el podcast.
+
+
+8) Como usuario puedo poder reservar los servicios de otro usuario.
+
+
+9) Como usuario debe poder guardar a otros usuarios y saber en donde realizarán una performance en vivo. (Coming event)
+
+10) Como usuario debe poder editar una sección para poder publicar el espacio (lugar y hora) donde haré stand up comedy.(performance)
+
+11) como usuario puedo “generar nuevos” podcast. Para luego publicarlos en la plataforma.
+
+
+
+=end
